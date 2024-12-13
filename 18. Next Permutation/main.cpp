6544143d8aa4b7c677ce5eded16f34b7442b5a77
Shafayet <11shafayet@gmail.com>
2024-12-13 20:13:26 +0600

    added 18.

--- 18. Next Permutation/main.cpp
+++ 18. Next Permutation/main.cpp
@@ -1,65 +1,65 @@
-
-// https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1
+// https://leetcode.com/problems/next-permutation/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-
 class Solution {
-  public:
-    // arr : given array
-    // k : find kth smallest element and return using this function
-    int kthSmallest(vector<int> &arr, int k) {
-      // find the maximum value from the array
-      int maxValue = *max_element(arr.begin(), arr.end());
-      
-      // create an array with the length of maxValue + 1 and initialize all the value to 0
-      vector<int> myArray(maxValue + 1, 0);
-      
-      // traverse the main arr and increase the number by 1 in myArray
-      for(auto num : arr) {
-          myArray[num]++;
-      }
-      
-      //  find the kth element as the list is sorted
-      int count = 0;
-      for(int i = 0; i <= maxValue; i++) {
-          count += myArray[i];
-          if(count >= k) {
-              return i;
-          }
-      }
-      
-      return -1;
+public:
+    void nextPermutation(vector<int>& nums) {
+       // brute force approach : 
+        // find all permutations,
+        // search for our permutation
+        // return the next one
+
+        ////////////// optimal approach ////////////////////////
+        int pivot = -1, n = nums.size();
+
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if(pivot == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for(int i = n - 1; i > pivot; i--) {
+            if(nums[i] > nums[pivot]) {
+                swap(nums[i], nums[pivot]);
+                break;
+            }
+        }
+         // reverse(nums.begin() + pivot + 1, nums.end());
+
+        int i = pivot + 1, j = n - 1;
+        while(i < j) {
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
     }
 };
 
-
 int main() {
-    int test_case;
-    cin >> test_case;  
-    cin.ignore();  
-    while (test_case--) {
-        int k;
-        vector<int> arr;
-        string input;
-        
-        // Read the array of numbers (space-separated)
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        
-        // Read the value of k
-        getline(cin, input);
-        stringstream ss2(input);
-        ss2 >> k;
-        
-        Solution ob;
-        cout << ob.kthSmallest(arr, k) << endl;
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
     }
+
+    Solution solution;
+    solution.nextPermutation(nums);
+
+    cout << "Next permutation: ";
+    for(int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
     return 0;
 }
-
