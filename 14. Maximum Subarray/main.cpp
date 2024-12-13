b00f80285e771dc1773732c8c877e71e307261c9
Shafayet <11shafayet@gmail.com>
2024-12-13 20:08:01 +0600

    added 14.

--- 14. Maximum Subarray/main.cpp
+++ 14. Maximum Subarray/main.cpp
@@ -1,65 +1,59 @@
 
-// https://www.geeksforgeeks.org/problems/kth-smallest-element5635/1
+// https://leetcode.com/problems/maximum-subarray/description/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
 class Solution {
-  public:
-    // arr : given array
-    // k : find kth smallest element and return using this function
-    int kthSmallest(vector<int> &arr, int k) {
-      // find the maximum value from the array
-      int maxValue = *max_element(arr.begin(), arr.end());
-      
-      // create an array with the length of maxValue + 1 and initialize all the value to 0
-      vector<int> myArray(maxValue + 1, 0);
-      
-      // traverse the main arr and increase the number by 1 in myArray
-      for(auto num : arr) {
-          myArray[num]++;
-      }
-      
-      //  find the kth element as the list is sorted
-      int count = 0;
-      for(int i = 0; i <= maxValue; i++) {
-          count += myArray[i];
-          if(count >= k) {
-              return i;
-          }
-      }
-      
-      return -1;
+public:
+    int maxSubArray(vector<int>& nums) {
+        /////////////////////// Brute Force Approach /////////////////////////////
+
+        // int n = nums.size();
+        // int maxSum = INT_MIN;
+
+        // for(int i = 0; i < n; i++) {
+        //     int currentSum = 0;
+        //     for(int j = i; j < n; j++) {
+        //         currentSum += nums[j];
+        //         maxSum = max(currentSum, maxSum);
+        //     }
+        // }
+
+        // return maxSum;
+
+
+        ///////////////////// With Kadane's Algo ///////////////////////////////////
+        int n = nums.size();
+        int currentSum = 0;
+        int maxSum = INT_MIN;
+
+        for(int num : nums) {
+            currentSum += num;
+            maxSum = max(currentSum, maxSum);
+            if(currentSum < 0) currentSum = 0;
+        }
+
+        return maxSum;
     }
 };
 
 
 int main() {
-    int test_case;
-    cin >> test_case;  
-    cin.ignore();  
-    while (test_case--) {
-        int k;
-        vector<int> arr;
-        string input;
-        
-        // Read the array of numbers (space-separated)
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        
-        // Read the value of k
-        getline(cin, input);
-        stringstream ss2(input);
-        ss2 >> k;
-        
-        Solution ob;
-        cout << ob.kthSmallest(arr, k) << endl;
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
     }
+
+    Solution solution;
+    int result = solution.maxSubArray(nums);
+
+    cout << "Maximum sum of subarray: " << result << endl;
+
     return 0;
 }
 
