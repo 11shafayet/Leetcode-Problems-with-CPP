699c6840f287159c721422cd5be2737f87074f11
Shafayet <11shafayet@gmail.com>
2025-04-13 12:29:33 +0600

    Leetcode "14. Longest Common Prefix"

--- 14. Longest Common Prefix/main.cpp
+++ 14. Longest Common Prefix/main.cpp
@@ -1,14 +1,26 @@
-// https://leetcode.com/problems/rotate-string/description/
+// https://leetcode.com/problems/longest-common-prefix/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()) return false;
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string ans = strs[0];
 
-        return (s+s).find(goal) != string::npos;
+        for(int j = 1; j < strs.size(); j++) {
+            int i = 0;
+
+            while(i < ans.length() && i < strs[j].length() && ans[i] == strs[j][i]) {
+                i++;
+            }
+
+            ans = ans.substr(0, i);
+            if(ans.empty()) return ans;
+        }
+
+        return ans;
     }
 };
 
@@ -16,12 +28,15 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        string s, t;
-        cin >> s >> t;
-  
-        Solution ob;
-        cout << ob.rotateString(s,t) << endl;
+        int n;
+        cin >> n; // number of strings in the vector
+        vector<string> strs(n);
+        for (int i = 0; i < n; i++) {
+            cin >> strs[i];
+        }
 
+        Solution ob;
+        cout << ob.longestCommonPrefix(strs) << endl;
         cout << "~" << endl;
     }
     return 0;
